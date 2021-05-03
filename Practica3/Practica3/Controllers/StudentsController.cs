f61227b410c823c4a3e61418b49ff679e3ef7323
Gabriel <gabogabygpp16@gmail.com>
2021-05-03 11:51:37 -0400

    P3-001 First steps

---
+++ Practica3/Practica3/Controllers/StudentsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Practica2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        public StudentsController()
+        {
+        }
+
+        [HttpGet]
+        public List<Student> GetStudents()
+        {
+            return new List<Student>()
+            {
+                new Student() { Name = "Gabriel", LastName = "Perez"},
+                new Student() { Name = "Mauricio", LastName = "Terceros"},
+                new Student() { Name = "Sofia", LastName = "Vargas"},
+                new Student() { Name = "Alejandro", LastName = "Ledezma"},
+                new Student() { Name = "Carlos", LastName = "Coronado"},
+                new Student() { Name = "Boris", LastName = "Pedraza"},
+                new Student() { Name = "Mauricio", LastName = "Angulo"},
+                new Student() { Name = "Fabri", LastName = "Fernandez"},
+            };
+        }
+
+        [HttpPost]
+        public Student CreateStudent([FromBody] String studentName, String studentLastName)
+        {
+            return new Student()
+            {
+                Name = studentName,
+                LastName = studentLastName
+            };
+        }
+
+        [HttpPut]
+        public Student UpdateStudent([FromBody] Student student)
+        {
+            student.Name = "updated";
+            student.LastName = "updated";
+            return student;
+        }
+
+        [HttpDelete]
+        public Student DeleteStudent([FromBody] Student student)
+        {
+            student.Name = "deleted";
+            student.LastName = "deleted";
+            return student;
+        }
+    }
+}
---
+++ Practica3/Practica3/Students.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Practica2
+{
+    public class Student
+    {
+        public String Name { get; set; }
+        public String LastName { get; set; }
+    }
+}
